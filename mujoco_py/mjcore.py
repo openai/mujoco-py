063d99ead09df8adc7ca3b364e6a7d8614de2454
Michael Mathew <michaelmathew@Michaels-MacBook-Pro.local>
2016-10-16 03:26:37 +0100

    few changes

--- mujoco_py/mjcore.py
+++ mujoco_py/mjcore.py
@@ -69,7 +69,18 @@ class MjModel(MjModelWrapper):
         return fullqM
     
     def copy(self, data_d, data_s, length):
+        print "d b4\t",data_d
         mjlib.mju_copy(data_d.ctypes.data_as(POINTER(c_double)), data_s.ctypes.data_as(POINTER(c_double)), c_int(length))
+        print "d \t", data_d
+        print "s \t", data_s
+
+#this function copies values by overwriting the memory location. So be careful with the length.
+#if not it can overwrite some locations that is not intended leading to some unwanted mishaps
+    def deepcopy(self, data_d, data_s, length, idx):
+        print "destination \t", data_d
+        print "source \t", data_s
+        ctypes.memmove(self._wrapped.contents.body_pos[idx], data_s.ctypes.data_as(POINTER(c_double)), length * sizeof(c_double))
+        print "destin ptr after \t",data_d
 
     def step1(self):
         mjlib.mj_step(self.ptr, self.data.ptr)
@@ -130,44 +141,56 @@ class MjModel(MjModelWrapper):
         return [ctypes.string_at(start_addr + int(inc))
                 for inc in self.name_jntadr.flatten()]
 
-    def geom_location(self, geom_name):
-        geom_adr = mjlib.mj_name2id(self.ptr, C.mjOBJ_GEOM, geom_name);
+    def geom_name2id(self, geom_name):
+        return mjlib.mj_name2id(self.ptr, C.mjOBJ_GEOM, geom_name)
+    
+    def site_name2id(self, site_name):
+        return mjlib.mj_name2id(self.ptr, C.mjOBJ_SITE, site_name)
+    
+    def body_name2id(self, body_name):
+        return mjlib.mj_name2id(self.ptr, C.mjOBJ_BODY, body_name)
+
+    def joint_name2id(self, joint_name):
+        return mjlib.mj_name2id(self.ptr, C.mjOBJ_JOINT, joint_name)
+
+    def geom_pose(self, geom_name):
+        geom_adr = self.geom_name2id(geom_name);
         assert(geom_adr >= 0);
         geom_loc = self.data.geom_xpos[geom_adr]
-	geom_ori = self.data.geom_xmat[body_adr]
+        geom_ori = self.data.geom_xmat[geom_adr]
         return (geom_loc, geom_ori)
     
-    def site_location(self, site_name):
-        site_adr = mjlib.mj_name2id(self.ptr, C.mjOBJ_SITE, site_name);
+    def site_pose(self, site_name):
+        site_adr = self.site_name2id(site_name);
         assert(site_adr >= 0);
         site_loc = self.data.site_xpos[site_adr]
-	site_ori = self.data.site_xmat[body_adr]
+        site_ori = self.data.site_xmat[site_adr]
         return (site_loc, site_ori)
     
-    def body_location(self, body_name):
-        body_adr = mjlib.mj_name2id(self.ptr, C.mjOBJ_BODY, body_name);
+    def body_pose(self, body_name):
+        body_adr = self.body_name2id(body_name);
         assert(body_adr >= 0);
         body_loc = self.data.xpos[body_adr]
         body_ori = self.data.xquat[body_adr]
         return (body_loc, body_ori)
 
     def body_vel(self, body_name):
-        body_adr = mjlib.mj_name2id(self.ptr, C.mjOBJ_BODY, body_name);
+        body_adr = self.body_name2id(body_name);
         assert(body_adr >= 0);
         vel = self.data.cvel[body_adr];
         body_vel_tran = vel[3:6];
         body_vel_rot = vel[0:3];
         return (body_vel_tran, body_vel_rot)
     
-    def joint_location(self, joint_name):
-        joint_adr = mjlib.mj_name2id(self.ptr, C.mjOBJ_JOINT, joint_name);
+    def joint_pose(self, joint_name):
+        joint_adr = self.joint_name2id(joint_name);
         assert(joint_adr >= 0);
         joint_loc = self.data.xpos[joint_adr]
         joint_ori = self.data.xquat[joint_adr]
         return (joint_loc, joint_ori)
 
     def joint_vel(self, joint_name):
-        joint_adr = mjlib.mj_name2id(self.ptr, C.mjOBJ_JOINT, joint_name);
+        joint_adr = self.joint_name2id(joint_name);
         assert(joint_adr >= 0);
         vel = self.data.cvel[joint_adr];
         joint_vel_tran = vel[3:6];
@@ -176,11 +199,11 @@ class MjModel(MjModelWrapper):
 
 #compute 3/6-by-nv Jacobian of global point attached to given body
     def jac(self, world_coord, body_name):
-        body_adr = mjlib.mj_name2id(self.ptr, C.mjOBJ_BODY, body_name);
+        body_adr = self.body_name2id(body_name);
         assert(body_adr >= 0);
         jacp = np.zeros((3,self.nv), dtype=np.double)
         jacr = np.zeros((3,self.nv), dtype=np.double)
-	self.forward()
+        self.forward()
         mjlib.mj_jac(self.ptr,\
                      self.data.ptr,\
                      jacp.ctypes.data_as(POINTER(c_double)),\
@@ -191,11 +214,11 @@ class MjModel(MjModelWrapper):
 
 #compute body frame Jacobian
     def jacBody(self, body_name):
-        body_adr = mjlib.mj_name2id(self.ptr, C.mjOBJ_BODY, body_name);
+        body_adr = self.body_name2id(body_name);
         assert(body_adr >= 0);
         jacp = np.zeros((3,self.nv), dtype=np.double)
         jacr = np.zeros((3,self.nv), dtype=np.double)
-	self.forward()
+        self.forward()
         mjlib.mj_jacBody(self.ptr,\
                          self.data.ptr,\
                          jacp.ctypes.data_as(POINTER(c_double)),\
@@ -205,11 +228,11 @@ class MjModel(MjModelWrapper):
 
 #compute body center-of-mass Jacobian
     def jacBodyCom(self, body_name):
-        body_adr = mjlib.mj_name2id(self.ptr, C.mjOBJ_BODY, body_name);
+        body_adr = self.body_name2id(body_name);
         assert(body_adr >= 0);
         jacp = np.zeros((3,self.nv), dtype=np.double)
         jacr = np.zeros((3,self.nv), dtype=np.double)
-	self.forward()
+        self.forward()
         mjlib.mj_jacBodyCom(self.ptr,\
                             self.data.ptr,\
                             jacp.ctypes.data_as(POINTER(c_double)),\
@@ -219,11 +242,11 @@ class MjModel(MjModelWrapper):
 
 #compute geom Jacobian
     def jacGeom(self, geom_name):
-        geom_adr = mjlib.mj_name2id(self.ptr, C.mjOBJ_GEOM, geom_name);
+        geom_adr = self.geom_name2id(geom_name);
         assert(geom_adr >= 0);
         jacp = np.zeros((3,self.nv), dtype=np.double)
         jacr = np.zeros((3,self.nv), dtype=np.double)
-	self.forward()
+        self.forward()
         mjlib.mj_jacGeom(self.ptr,\
                          self.data.ptr,\
                          jacp.ctypes.data_as(POINTER(c_double)),\
@@ -233,11 +256,11 @@ class MjModel(MjModelWrapper):
 
 #compute site Jacobian
     def jacSite(self, site_name):
-        site_adr = mjlib.mj_name2id(self.ptr, C.mjOBJ_GEOM, site_name);
+        site_adr = self.site_name2id(site_name);
         assert(site_adr >= 0);
         jacp = np.zeros((3,self.nv), dtype=np.double)
         jacr = np.zeros((3,self.nv), dtype=np.double)
-	self.forward()
+        self.forward()
         mjlib.mj_jacSite(self.ptr,\
                          self.data.ptr,\
                          jacp.ctypes.data_as(POINTER(c_double)),\
@@ -247,7 +270,7 @@ class MjModel(MjModelWrapper):
 
 # compute translation Jacobian of point, and rotation Jacobian of axis
     def jacPointAxis(self, point, axis, body_name):
-        body_adr = mjlib.mj_name2id(self.ptr, C.mjOBJ_BODY, body_name);
+        body_adr = self.body_name2id(body_name);
         assert(body_adr >= 0);
         jacp = np.zeros((3,1), dtype=np.double)
         jaca = np.zeros((3,1), dtype=np.double)
@@ -263,24 +286,24 @@ class MjModel(MjModelWrapper):
 
 #apply cartesian force and torque (outside xfrc_applied mechanism)
     def applyFT(self, point, force, torque, body_name):
-	body_adr = mjlib.mj_name2id(self.ptr, C.mjOBJ_BODY, body_name);
+        body_adr = self.body_name2id(body_name);
         assert(body_adr >= 0);
-	qfrc_target = np.zeros((self.nv,1), dtype=np.double)
-	mjlib.mj_applyFT(self.ptr,\
+        qfrc_target = np.zeros((self.nv,1), dtype=np.double)
+        mjlib.mj_applyFT(self.ptr,\
                          self.data.ptr,\
                          force.ctypes.data_as(POINTER(c_double)),\
                          torque.ctypes.data_as(POINTER(c_double)),\
                          point.ctypes.data_as(POINTER(c_double)),\
                          body_adr,\
                          qfrc_target[(body_adr-1)*6].ctypes.data_as(POINTER(c_double)));
-	return qfrc_target
+        return qfrc_target
 
     def joint_adr(self, joint_name):
         """Return (qposadr, qveladr, dof) for the given joint name.
 
         If dof is 4 or 7, then the last 4 degrees of freedom in qpos represent a
         unit quaternion."""
-        jntadr = mjlib.mj_name2id(self.ptr, C.mjOBJ_JOINT, joint_name)
+        jntadr = self.joint_name2id(joint_name)
         assert(jntadr >= 0)
         dofmap = {C.mjJNT_FREE:  7,
                   C.mjJNT_BALL:  4,
