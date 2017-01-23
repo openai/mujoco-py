97f8ddac8ab0c4ee9f0058da630c4a03645bf866
Bob McGrew <bmcgrew@cs.stanford.edu>
2017-01-23 13:05:11 -0800

    Expose actuator names in the MjModel class.

--- mujoco_py/mjcore.py
+++ mujoco_py/mjcore.py
@@ -137,6 +137,12 @@ class MjModel(MjModelWrapper):
         return [ctypes.string_at(start_addr + int(inc))
                 for inc in self.name_numericadr.flatten()]
 
+    @property
+    def actuator_names(self):
+        start_addr = ctypes.addressof(self.names.contents)
+        return [ctypes.string_at(start_addr + int(inc))
+                for inc in self.name_actuatoradr.flatten()]
+
 
 class MjData(MjDataWrapper):
 
