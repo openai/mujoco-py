943ce1494c736e056f95234cc00200a9d8847ace
Bob McGrew <bmcgrew@openai.com>
2017-01-25 13:06:30 -0800

    Added camera_names as property.

--- mujoco_py/mjcore.py
+++ mujoco_py/mjcore.py
@@ -143,6 +143,12 @@ class MjModel(MjModelWrapper):
         return [ctypes.string_at(start_addr + int(inc))
                 for inc in self.name_actuatoradr.flatten()]
 
+    @property
+    def camera_names(self):
+        start_addr = ctypes.addressof(self.names.contents)
+        return [ctypes.string_at(start_addr + int(inc))
+                for inc in self.name_camadr.flatten()]
+
 
 class MjData(MjDataWrapper):
 
