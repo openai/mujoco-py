b717148bc6a9da2e6d5416d301e73a6ae74773ea
Peter Welinder <peter@welinder.se>
2017-07-26 16:10:19 -0700

    Add dummy methods for osmesashim

--- mujoco_py/gl/osmesashim.c
+++ mujoco_py/gl/osmesashim.c
@@ -57,3 +57,23 @@ void closeOpenGL() {
         is_initialized = 0;
     }
 }
+
+unsigned int createPBO(int width, int height, int batchSize, int use_short) {
+    return 0;
+}
+
+void freePBO(unsigned int pixelBuffer) {
+}
+
+void copyFBOToPBO(mjrContext* con,
+                  unsigned int pbo_rgb, unsigned int pbo_depth,
+                  mjrRect viewport, int bufferOffset) {
+}
+
+void readPBO(unsigned char *buffer_rgb, unsigned short *buffer_depth,
+             unsigned int pbo_rgb, unsigned int pbo_depth,
+             int width, int height, int batchSize) {
+}
+
+void syncOpenGL() {
+}
