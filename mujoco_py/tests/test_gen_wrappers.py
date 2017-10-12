6032ba23d0b900c58e4d03fe9344ae58b5aaba18
Alex Ray <a@machinaut.com>
2017-10-12 14:24:59 -0700

    add test to verify wrappers match

--- mujoco_py/tests/test_gen_wrappers.py
+++ mujoco_py/tests/test_gen_wrappers.py
@@ -2,6 +2,7 @@ import subprocess
 import sys
 import os
 import pytest
+import mujoco_py
 
 
 @pytest.mark.skipif(sys.platform.startswith("win"), reason="This test fails on windows.")
@@ -13,6 +14,7 @@ def test_gen_wrappers():
     return fname
 
 
+@pytest.mark.skipif(sys.platform.startswith("win"), reason="This test fails on windows.")
 def test_deterministic():
     # Verifies that gen_wrappers is deterministic
     fname = test_gen_wrappers()
@@ -21,3 +23,15 @@ def test_deterministic():
     fname = test_gen_wrappers()
     with open(fname) as f:
         assert f.read() == s, 'Generate wrappers returned different result'
+
+
+@pytest.mark.skipif(sys.platform.startswith("win"), reason="This test fails on windows.")
+def test_generated():
+    # Verifies generated wrappers match checked-in wrappers
+    fname = test_gen_wrappers()
+    with open(fname) as f:
+        generated_str = f.read()
+    gname = os.path.join(os.path.dirname(mujoco_py.cymj.__file__), 'wrappers.pxi')
+    with open(gname) as f:
+        checkedin_str = f.read()
+    assert generated_str == checkedin_str, 'Generated wrappers do not match'
