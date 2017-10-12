b102ffd993f0cc3468da2ced16315d54f937e0fa
Alex Ray <a@machinaut.com>
2017-10-12 14:20:18 -0700

    add test for deterministic generation

--- mujoco_py/tests/test_gen_wrappers.py
+++ mujoco_py/tests/test_gen_wrappers.py
@@ -7,9 +7,17 @@ import pytest
 @pytest.mark.skipif(sys.platform.startswith("win"), reason="This test fails on windows.")
 def test_gen_wrappers():
     # Verifies that gen_wrappers can be executed.
-    if sys.platform.startswith("win"):
-        fname = "C:\generated_wrappers.pxi"
-    else:
-        fname = "/tmp/generated_wrappers.pxi"
+    fname = "/tmp/generated_wrappers.pxi"
     subprocess.check_call([sys.executable,
                            os.path.join("scripts", "gen_wrappers.py"), fname])
+    return fname
+
+
+def test_deterministic():
+    # Verifies that gen_wrappers is deterministic
+    fname = test_gen_wrappers()
+    with open(fname) as f:
+        s = f.read()
+    fname = test_gen_wrappers()
+    with open(fname) as f:
+        assert f.read() == s, 'Generate wrappers returned different result'
