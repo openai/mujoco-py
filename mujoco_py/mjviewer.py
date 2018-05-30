29f0ef42ecd5e818373c763f179a1d6fe10374be
iTakeshi <itakeshi.net@gmail.com>
2018-05-30 14:44:03 +0900

    set arbitrary resolution for rendering

--- mujoco_py/mjviewer.py
+++ mujoco_py/mjviewer.py
@@ -205,10 +205,11 @@ class MjViewer(MjViewerBasic):
         self._markers[:] = []
         self._overlay.clear()
 
-    def _read_pixels_as_in_window(self):
-        # Reads pixels with markers and overlay from the same camera as screen.
-        resolution = glfw.get_framebuffer_size(
-            self.sim._render_context_window.window)
+    def _read_pixels_as_in_window(self, resolution=None):
+        if resolution is None:
+            # Reads pixels with markers and overlay from the same camera as screen.
+            resolution = glfw.get_framebuffer_size(
+                self.sim._render_context_window.window)
 
         resolution = np.array(resolution)
         resolution = resolution * min(1000 / np.min(resolution), 1)
