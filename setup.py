7ae23726d2eebaa34e21bf3075b372101b316509
Alex Ray <a@machinaut.com>
2018-10-11 12:33:08 -0700

    closer to master

--- setup.py
+++ setup.py
@@ -1,17 +1,6 @@
 #!/usr/bin/env python3
 from os.path import join, dirname, realpath
-from os import getenv
 from setuptools import find_packages, setup
-from setuptools.extension import Extension
-from Cython.Build import cythonize
-from ctypes import cdll
-import numpy as np
-
-MJ_PATH = join(getenv('HOME'), '.mujoco', 'mjpro150')
-
-# XXX this is mac specific for now
-cdll.LoadLibrary(join(MJ_PATH, 'bin', 'libmujoco150.dylib'))
-cdll.LoadLibrary(join(MJ_PATH, 'bin', 'libglfw.3.dylib'))
 
 
 with open(join("mujoco_py", "version.py")) as version_file:
@@ -30,22 +19,6 @@ for p in packages:
     assert p == 'mujoco_py' or p.startswith('mujoco_py.')
 
 
-cymjpyx = join('mujoco_py', 'cymj.pyx')
-include = join(MJ_PATH, 'include')
-mjbin = join(MJ_PATH, 'bin')
-print('include', include)
-
-
-cymj_extension = Extension(name='mujoco_py.cymj',
-                           sources=['mujoco_py/cymj.pyx'],
-                           include_dirs=[include, np.get_include()],
-                           library_dirs=[mjbin],
-                           libraries=['mujoco150', 'glfw.3'])
-
-
-ext_modules = [cymj_extension]
-
-
 setup(
     name='mujoco-py',
     version=__version__,  # noqa
@@ -56,7 +29,6 @@ setup(
     include_package_data=True,
     package_dir={'mujoco_py': 'mujoco_py'},
     package_data={'mujoco_py': ['generated/*.so']},
-    ext_modules=cythonize(ext_modules),
     install_requires=read_requirements_file('requirements.txt'),
     tests_require=read_requirements_file('requirements.dev.txt'),
 )
