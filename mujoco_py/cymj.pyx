da47d6c6686c92f8c01ca8bd7d56b70e2c4eab04
Bowen Baker <bobabeke@gmail.com>
2019-08-12 13:53:02 -0700

    Delete xml after loading (#411)

--- mujoco_py/cymj.pyx
+++ mujoco_py/cymj.pyx
@@ -183,10 +183,10 @@ def load_model_from_xml(str xml_str):
     cdef char errstr[300]
     cdef mjModel *model
     with wrap_mujoco_warning():
-        with tempfile.NamedTemporaryFile(suffix=".xml", delete=False) as fp:
+        with tempfile.NamedTemporaryFile(suffix=".xml", delete=True) as fp:
             fp.write(xml_str.encode())
             fp.flush()
-        model = mj_loadXML(fp.name.encode(), NULL, errstr, 300)
+            model = mj_loadXML(fp.name.encode(), NULL, errstr, 300)
     if model == NULL:
         raise Exception('%s\nFailed to load XML from file: %s. mj_loadXML error: %s' % (xml_str, fp.name, errstr,))
     return WrapMjModel(model)
