a4aa1b93a2025681587e9173191e9332e20d0029
Jerry Tworek <jerry@millionintegrals.com>
2019-08-12 14:27:14 -0700

    Clean up temporary files.

--- mujoco_py/cymj.pyx
+++ mujoco_py/cymj.pyx
@@ -188,7 +188,7 @@ def load_model_from_xml(str xml_str):
             fp.flush()
             model = mj_loadXML(fp.name.encode(), NULL, errstr, 300)
     if model == NULL:
-        raise Exception('%s\nFailed to load XML from file: %s. mj_loadXML error: %s' % (xml_str, fp.name, errstr,))
+        raise Exception('%s\nFailed to load XML from string. mj_loadXML error: %s' % (xml_str, errstr,))
     return WrapMjModel(model)
 
 
