83759c2e20df965dcf727aa79e590a0483aaea0f
Tao Xu <taoxu76@gmail.com>
2019-10-08 16:40:16 -0700

    PID implementation inside mujoco-py (#462)
    * PID implementation inside mujoco-py
    
    * addressing the comments from Jerry and Arthur
    
    * add unit tests for new PID control
    
    * add one more unit test for backward compatibility
    
    * addressing comments

---
+++ mujoco_py/mjpid.pyx
@@ -0,0 +1,97 @@
+from libc.math cimport fabs, fmax, fmin
+from mujoco_py.generated import const
+
+"""
+  Kp == Kp
+  Ki == Kp/Ti
+  Kd == Kp*Td
+
+  In this situation, Kp is a knob to tune the agressiveness, wheras Ti and Td will
+  change the response time of the system in a predictable way. Lower Ti or Td means
+  that the system will respond to error more quickly/agressively.
+
+  error deadband: if set will shrink error within to 0.0
+  clamp on integral term:  helps on saturation problem in I.
+  derivative smoothing term:  reduces high frequency noise in D.
+
+  set in gainprm="Kp Ti Td iClamp errBand iSmooth" in mujoco xml.
+"""
+cdef enum USER_DEFINED_ACTUATOR_PARAMS:
+    IDX_PROPORTIONAL_GAIN = 0,
+    IDX_INTEGRAL_TIME_CONSTANT = 1,
+    IDX_INTEGRAL_MAX_CLAMP = 2,
+    IDX_DERIVATIVE_TIME_CONSTANT = 3,
+    IDX_DERIVATIVE_GAIN_SMOOTHING = 4,
+    IDX_ERROR_DEADBAND = 5,
+
+
+cdef enum USER_DEFINED_CONTROLLER_DATA:
+    IDX_INTEGRAL_ERROR = 0,
+    IDX_LAST_ERROR = 1,
+    IDX_DERIVATIVE_ERROR_LAST = 2,
+    NUM_USER_DATA_PER_ACT = 3,
+
+
+cdef mjtNum c_zero_gains(const mjModel* m, const mjData* d, int id) with gil:
+    return 0.0
+
+
+cdef mjtNum c_pid_bias(const mjModel* m, const mjData* d, int id) with gil:
+    cdef double dt_in_sec = m.opt.timestep
+    cdef double error = d.ctrl[id] - d.actuator_length[id]
+    cdef int NGAIN = int(const.NGAIN)
+
+    cdef double Kp = m.actuator_gainprm[id * NGAIN + IDX_PROPORTIONAL_GAIN]
+    cdef double error_deadband = m.actuator_gainprm[id * NGAIN + IDX_ERROR_DEADBAND]
+    cdef double integral_max_clamp = m.actuator_gainprm[id * NGAIN + IDX_INTEGRAL_MAX_CLAMP]
+    cdef double integral_time_const = m.actuator_gainprm[id * NGAIN + IDX_INTEGRAL_TIME_CONSTANT]
+    cdef double derivative_gain_smoothing = \
+        m.actuator_gainprm[id * NGAIN  + IDX_DERIVATIVE_GAIN_SMOOTHING]
+    cdef double derivate_time_const = m.actuator_gainprm[id * NGAIN + IDX_DERIVATIVE_TIME_CONSTANT]
+
+    cdef double effort_limit_low = m.actuator_forcerange[id * 2]
+    cdef double effort_limit_high = m.actuator_forcerange[id * 2 + 1]
+
+    if fabs(error) < error_deadband:
+        error = 0.0
+
+    integral_error = d.userdata[id * NUM_USER_DATA_PER_ACT + IDX_INTEGRAL_ERROR]
+    integral_error += error * dt_in_sec
+    integral_error = fmax(-integral_max_clamp, fmin(integral_max_clamp, integral_error))
+
+    last_error = d.userdata[id * NUM_USER_DATA_PER_ACT + IDX_LAST_ERROR]
+    cdef double derivative_error = (error - last_error) / dt_in_sec
+
+    derivative_error_last = d.userdata[id * NUM_USER_DATA_PER_ACT + IDX_DERIVATIVE_ERROR_LAST]
+
+    derivative_error = (1.0 - derivative_gain_smoothing) * derivative_error_last + \
+        derivative_gain_smoothing * derivative_error
+
+    cdef double integral_error_term = 0.0
+    if integral_time_const != 0:
+        integral_error_term = integral_error / integral_time_const
+
+    cdef double derivative_error_term = derivative_error * derivate_time_const
+
+    f = Kp * (error + integral_error_term + derivative_error_term)
+    # print(id, d.ctrl[id], d.actuator_length[id], error, integral_error_term, derivative_error_term,
+    #    derivative_error, dt_in_sec, last_error, integral_error, derivative_error_last, f)
+
+    d.userdata[id * NUM_USER_DATA_PER_ACT + IDX_LAST_ERROR] = error
+    d.userdata[id * NUM_USER_DATA_PER_ACT + IDX_DERIVATIVE_ERROR_LAST] = derivative_error
+    d.userdata[id * NUM_USER_DATA_PER_ACT + IDX_INTEGRAL_ERROR] = integral_error
+    return fmax(effort_limit_low, fmin(effort_limit_high, f))
+
+
+def set_pid_control(m, d):
+    global mjcb_act_gain
+    global mjcb_act_bias
+
+    if m.nuserdata < m.nu * NUM_USER_DATA_PER_ACT:
+        raise Exception('nuserdata is not set large enough to store PID internal states')
+
+    for i in range(m.nuserdata):
+        d.userdata[i] = 0.0
+
+    mjcb_act_gain = c_zero_gains
+    mjcb_act_bias = c_pid_bias
---
+++ mujoco_py/tests/test_pid.py
@@ -0,0 +1,94 @@
+import numpy as np
+import pytest
+import random
+from mujoco_py import (MjSim, load_model_from_xml, cymj)
+
+MODEL_XML = """
+<mujoco model="inverted pendulum">
+	<size nuserdata="100"/>
+	<compiler inertiafromgeom="true"/>
+	<default>
+		<joint armature="0" damping="1" limited="true"/>
+		<geom contype="0" friction="1 0.1 0.1" rgba="0.7 0.7 0 1"/>
+		<tendon/>
+		<motor ctrlrange="-3 3"/>
+	</default>
+	<option gravity="0 0 -9.81" integrator="RK4" timestep="0.001"/>
+	<size nstack="3000"/>
+	<worldbody>
+		<geom name="rail" pos="0 0 0" quat="0.707 0 0.707 0" rgba="0.3 0.3 0.7 1" size="0.02 1" type="capsule"/>
+		<body name="cart" pos="0 0 0">
+			<geom name="cart" pos="0 0 0" quat="0.707 0 0.707 0" size="0.1 0.1" type="capsule"/>
+			<body name="pole" pos="0 0 0">
+				<joint axis="0 1 0" name="hinge" pos="0 0 0" range="-90 90" type="hinge"/>
+				<geom fromto="0 0 0 0.001 0 0.6" name="cpole" rgba="0 0.7 0.7 1" size="0.049 0.3" type="capsule"/>
+			</body>
+		</body>
+	</worldbody>
+	<actuator>
+		{actuator}
+	</actuator>
+</mujoco>
+"""
+
+PID_ACTUATOR = """
+	<general ctrlrange='-1 1' gaintype="user" biastype="user" forcerange="-100 100" gainprm="200 10 10.0 0.1 0.1 0" joint="hinge" name="a-hinge"/>
+"""
+
+P_ONLY_ACTUATOR = """
+	<general ctrlrange='-1 1' gaintype="user" biastype="user" forcerange="-100 100" gainprm="200" joint="hinge" name="a-hinge"/>
+"""
+
+POSITION_ACTUATOR = """
+	<position ctrlrange='-1 1' forcerange="-100 100" kp=200 joint="hinge" name="a-hinge"/>
+"""
+
+
+"""
+	To enable PID control in the mujoco, please
+	refer to the setting in the PID_ACTUATOR.
+
+	Here we set Kp = 200, Ti = 10, Td = 0.1 (also iClamp = 10.0, dSmooth be 0.1)
+"""
+def test_mj_pid():
+	xml = MODEL_XML.format(actuator=PID_ACTUATOR)
+	model = load_model_from_xml(xml)
+	sim = MjSim(model)
+	cymj.set_pid_control(sim.model, sim.data)
+
+	# pertubation of pole to be unbalanced
+	init_pos = 0.1 * (random.random() - 0.5)
+	print('init pos', init_pos)
+	sim.data.qpos[0] = init_pos
+
+	pos = 0.0
+	sim.data.ctrl[0] = pos
+	print('desire position:', pos)
+
+	for _ in range(100):
+		sim.step()
+
+	print('final pos', sim.data.qpos[0])
+	assert abs(sim.data.qpos[0] - pos) < 0.01
+
+"""
+    check new PID control is backward compatible with  position control
+	when only has Kp term.
+"""
+def test_mj_proptional_only():
+	model = load_model_from_xml(MODEL_XML.format(actuator=P_ONLY_ACTUATOR))
+	sim = MjSim(model)
+	cymj.set_pid_control(sim.model, sim.data)
+
+	model2 = load_model_from_xml(MODEL_XML.format(actuator=POSITION_ACTUATOR))
+	sim2 = MjSim(model2)
+
+	init_pos = 0.1 * (random.random() - 0.5)
+	sim.data.qpos[0] = sim2.data.qpos[0] = init_pos
+	sim.data.ctrl[0] = sim2.data.ctrl[0] = 0
+
+	for i in range(20):
+		print(i, sim.data.qpos[0], sim2.data.qpos[0])
+		sim.step()
+		sim2.step()
+		assert abs(sim.data.qpos[0] - sim2.data.qpos[0]) <= 1e-5, "%d step violates" % i
