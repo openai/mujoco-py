83a6d60f85410ca1256062a4fd70beb4ecf8b03f
Tao Xu <tao@openai.com>
2019-10-08 15:48:08 -0700

    addressing comments

--- mujoco_py/mjpid.pyx
+++ mujoco_py/mjpid.pyx
@@ -10,25 +10,25 @@ from mujoco_py.generated import const
   change the response time of the system in a predictable way. Lower Ti or Td means
   that the system will respond to error more quickly/agressively.
 
-  error deadband: ERROR_DEADBAND if set will shrink error within to 0.0
-  clamp on integral term: INTEGRAL_MAX_CLAMP helps on saturation problem in I.
-  derivative smoothing term: DERIVATIVE_GAIN_SMOOTHING reduces high frequency noise in D.
+  error deadband: if set will shrink error within to 0.0
+  clamp on integral term:  helps on saturation problem in I.
+  derivative smoothing term:  reduces high frequency noise in D.
 
-  set in user="Ti Td iClamp errBand iSmooth" in mujoco xml.
+  set in gainprm="Kp Ti Td iClamp errBand iSmooth" in mujoco xml.
 """
 cdef enum USER_DEFINED_ACTUATOR_PARAMS:
-    PROPORTIONAL_GAIN = 0,
-    INTEGRAL_TIME_CONSTANT = 1,
-    INTEGRAL_MAX_CLAMP = 2,
-    DERIVATIVE_TIME_CONSTANT = 3,
-    DERIVATIVE_GAIN_SMOOTHING = 4,
-    ERROR_DEADBAND = 5,
+    IDX_PROPORTIONAL_GAIN = 0,
+    IDX_INTEGRAL_TIME_CONSTANT = 1,
+    IDX_INTEGRAL_MAX_CLAMP = 2,
+    IDX_DERIVATIVE_TIME_CONSTANT = 3,
+    IDX_DERIVATIVE_GAIN_SMOOTHING = 4,
+    IDX_ERROR_DEADBAND = 5,
 
 
 cdef enum USER_DEFINED_CONTROLLER_DATA:
-    INTEGRAL_ERROR = 0,
-    LAST_ERROR = 1,
-    DERIVATIVE_ERROR_LAST = 2,
+    IDX_INTEGRAL_ERROR = 0,
+    IDX_LAST_ERROR = 1,
+    IDX_DERIVATIVE_ERROR_LAST = 2,
     NUM_USER_DATA_PER_ACT = 3,
 
 
@@ -41,13 +41,13 @@ cdef mjtNum c_pid_bias(const mjModel* m, const mjData* d, int id) with gil:
     cdef double error = d.ctrl[id] - d.actuator_length[id]
     cdef int NGAIN = int(const.NGAIN)
 
-    cdef double Kp = m.actuator_gainprm[id * NGAIN + PROPORTIONAL_GAIN]
-    cdef double error_deadband = m.actuator_gainprm[id * NGAIN + ERROR_DEADBAND]
-    cdef double integral_max_clamp = m.actuator_gainprm[id * NGAIN + INTEGRAL_MAX_CLAMP]
-    cdef double integral_time_const = m.actuator_gainprm[id * NGAIN + INTEGRAL_TIME_CONSTANT]
+    cdef double Kp = m.actuator_gainprm[id * NGAIN + IDX_PROPORTIONAL_GAIN]
+    cdef double error_deadband = m.actuator_gainprm[id * NGAIN + IDX_ERROR_DEADBAND]
+    cdef double integral_max_clamp = m.actuator_gainprm[id * NGAIN + IDX_INTEGRAL_MAX_CLAMP]
+    cdef double integral_time_const = m.actuator_gainprm[id * NGAIN + IDX_INTEGRAL_TIME_CONSTANT]
     cdef double derivative_gain_smoothing = \
-        m.actuator_gainprm[id * NGAIN  + DERIVATIVE_GAIN_SMOOTHING]
-    cdef double derivate_time_const = m.actuator_gainprm[id * NGAIN + DERIVATIVE_TIME_CONSTANT]
+        m.actuator_gainprm[id * NGAIN  + IDX_DERIVATIVE_GAIN_SMOOTHING]
+    cdef double derivate_time_const = m.actuator_gainprm[id * NGAIN + IDX_DERIVATIVE_TIME_CONSTANT]
 
     cdef double effort_limit_low = m.actuator_forcerange[id * 2]
     cdef double effort_limit_high = m.actuator_forcerange[id * 2 + 1]
@@ -55,14 +55,14 @@ cdef mjtNum c_pid_bias(const mjModel* m, const mjData* d, int id) with gil:
     if fabs(error) < error_deadband:
         error = 0.0
 
-    integral_error = d.userdata[id * NUM_USER_DATA_PER_ACT + INTEGRAL_ERROR]
+    integral_error = d.userdata[id * NUM_USER_DATA_PER_ACT + IDX_INTEGRAL_ERROR]
     integral_error += error * dt_in_sec
     integral_error = fmax(-integral_max_clamp, fmin(integral_max_clamp, integral_error))
 
-    last_error = d.userdata[id * NUM_USER_DATA_PER_ACT + LAST_ERROR]
+    last_error = d.userdata[id * NUM_USER_DATA_PER_ACT + IDX_LAST_ERROR]
     cdef double derivative_error = (error - last_error) / dt_in_sec
 
-    derivative_error_last = d.userdata[id * NUM_USER_DATA_PER_ACT + DERIVATIVE_ERROR_LAST]
+    derivative_error_last = d.userdata[id * NUM_USER_DATA_PER_ACT + IDX_DERIVATIVE_ERROR_LAST]
 
     derivative_error = (1.0 - derivative_gain_smoothing) * derivative_error_last + \
         derivative_gain_smoothing * derivative_error
@@ -77,9 +77,9 @@ cdef mjtNum c_pid_bias(const mjModel* m, const mjData* d, int id) with gil:
     # print(id, d.ctrl[id], d.actuator_length[id], error, integral_error_term, derivative_error_term,
     #    derivative_error, dt_in_sec, last_error, integral_error, derivative_error_last, f)
 
-    d.userdata[id * NUM_USER_DATA_PER_ACT + LAST_ERROR] = error
-    d.userdata[id * NUM_USER_DATA_PER_ACT + DERIVATIVE_ERROR_LAST] = derivative_error
-    d.userdata[id * NUM_USER_DATA_PER_ACT + INTEGRAL_ERROR] = integral_error
+    d.userdata[id * NUM_USER_DATA_PER_ACT + IDX_LAST_ERROR] = error
+    d.userdata[id * NUM_USER_DATA_PER_ACT + IDX_DERIVATIVE_ERROR_LAST] = derivative_error
+    d.userdata[id * NUM_USER_DATA_PER_ACT + IDX_INTEGRAL_ERROR] = integral_error
     return fmax(effort_limit_low, fmin(effort_limit_high, f))
 
 
--- mujoco_py/tests/test_pid.py
+++ mujoco_py/tests/test_pid.py
@@ -31,44 +31,6 @@ MODEL_XML = """
 </mujoco>
 """
 
-
-MODEL_POSITION_ACTUATOR_XML = """
-<mujoco model="inverted pendulum">
-	<size nuserdata="100"/>
-	<compiler inertiafromgeom="true"/>
-	<default>
-		<joint armature="0" damping="1" limited="true"/>
-		<geom contype="0" friction="1 0.1 0.1" rgba="0.7 0.7 0 1"/>
-		<tendon/>
-		<motor ctrlrange="-3 3"/>
-	</default>
-	<option gravity="0 0 -9.81" integrator="RK4" timestep="0.001"/>
-	<size nstack="3000"/>
-	<worldbody>
-		<geom name="rail" pos="0 0 0" quat="0.707 0 0.707 0" rgba="0.3 0.3 0.7 1" size="0.02 1" type="capsule"/>
-		<body name="cart" pos="0 0 0">
-			<geom name="cart" pos="0 0 0" quat="0.707 0 0.707 0" size="0.1 0.1" type="capsule"/>
-			<body name="pole" pos="0 0 0">
-				<joint axis="0 1 0" name="hinge" pos="0 0 0" range="-90 90" type="hinge"/>
-				<geom fromto="0 0 0 0.001 0 0.6" name="cpole" rgba="0 0.7 0.7 1" size="0.049 0.3" type="capsule"/>
-			</body>
-		</body>
-	</worldbody>
-	<actuator>
-
-	</actuator>
-</mujoco>
-"""
-
-
-
-"""
-	To enable PID control in the mujoco, please
-	refer to the setting in the debug xml for the settings.
-
-	Here we set Kp = 200, Ti = 10, Td = 0.1 (also iClamp = 10.0, dSmooth be 0.1)
-"""
-
 PID_ACTUATOR = """
 	<general ctrlrange='-1 1' gaintype="user" biastype="user" forcerange="-100 100" gainprm="200 10 10.0 0.1 0.1 0" joint="hinge" name="a-hinge"/>
 """
@@ -81,6 +43,13 @@ POSITION_ACTUATOR = """
 	<position ctrlrange='-1 1' forcerange="-100 100" kp=200 joint="hinge" name="a-hinge"/>
 """
 
+
+"""
+	To enable PID control in the mujoco, please
+	refer to the setting in the PID_ACTUATOR.
+
+	Here we set Kp = 200, Ti = 10, Td = 0.1 (also iClamp = 10.0, dSmooth be 0.1)
+"""
 def test_mj_pid():
 	xml = MODEL_XML.format(actuator=PID_ACTUATOR)
 	model = load_model_from_xml(xml)
